3c458e6e199ec7ea0300a5758edfcd52cda97419
olagajkowska <67837503+olagajkowska@users.noreply.github.com>
2021-07-13 17:16:03 +0200

    Create WholeTheory.py

---
+++ WholeTheory.py
@@ -0,0 +1,28 @@
+from Whole import Whole
+from Theory import Theory
+from Tools import dudko_hummer_szabo
+
+
+class WholeTheory(Whole):
+
+    def __init__(self, filename, number_of_traces, **kwargs):
+        self._kwargs = kwargs
+        self._filename = filename
+        self._number_of_traces = number_of_traces
+        super().__init__(number_of_traces)
+
+    def _collect_traces(self):
+        return [Theory(self._filename[:2]+str(int(i)).zfill(2) + '.afm', i, **self._kwargs) for i in range(1,
+                                                                                                  self._number_of_traces)]
+
+    def _get_total_trace(self):
+        return Theory("total.txt", case=None, p=self.p_mean, k=self.k_mean)
+
+    def _analyze_total_trace(self):
+        self._total_trace.state_boundaries()
+        self._total_trace.plot(name="total_theory")
+        print(self._total_trace.histo_data)
+
+    def dudko(self):
+        dhs_table = dudko_hummer_szabo(self.rupture_table, self.states_rup)
+        return dhs_table
