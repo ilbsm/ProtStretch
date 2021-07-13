258cb3fa922f1b8566706033cd4a7ab98fcb5113
olagajkowska <67837503+olagajkowska@users.noreply.github.com>
2021-07-13 17:22:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,48 @@
 from Theory import Theory
 from Experiment import Experiment
 from WholeExperiment import WholeExperiment
+from WholeTheory import WholeTheory
 
-# trace = Theory("aa01.afm", 1)
-# trace.plot()
-# trace.state_boundaries()
-# trace.calculate_work()
-# trace.rupture_forces()
-# print(trace.histo_data)
-
+# PARAMETRY EKSPERYMENTU
 p_dna = 0.16
 l_dna = 350
 k_dna = 0.003
-# trace = Experiment("data_test.xls", 1, p_dna=p_dna, l_dna=l_dna, k_dna=k_dna)
-# trace.state_boundaries()
-# trace.plot()
-# # trace.state_boundaries()
-# trace.calculate_work()
-# trace.rupture_forces()
-# print(trace.histo_data)
 
-traces = WholeExperiment("data_test.xls", 10, p_dna=p_dna, l_dna=l_dna, k_dna=k_dna)
+#(ODKOMENTUJ TYLKO JEDEN Z PONIŻSZYCH BLOKÓW)
+
+#JAK ANALIZOWAĆ POJEDYNCZĄ TRAJEKTORIĘ Z SYMULACJI 
+trace = Theory("aa01.afm", 1)
+trace.state_boundaries()
+trace.plot()
+trace.calculate_work()
+trace.rupture_forces()
+print(trace.histo_data)
+
+#JAK ANALIZOWAĆ POJEDYNCZĄ TRAJEKTORIĘ Z EKSPERYMENTU
+
+trace = Experiment("data_test.xls", 1, p_dna=p_dna, l_dna=l_dna, k_dna=k_dna)
+trace.state_boundaries()
+trace.plot()
+trace.state_boundaries()
+trace.calculate_work()
+trace.rupture_forces()
+print(trace.histo_data)
+
+
+#JAK ANALIZOWAĆ ZESTAW TRAJEKTORII Z EKSPERYMENTU
+traces = WholeExperiment("data_test.xls", 21, p_dna=p_dna, l_dna=l_dna, k_dna=k_dna)
+traces.draw_rupture_histo()
+print(traces.rupture_table)
 traces.plot_all()
+res = traces.crooks()
+print(res)
+
+
+#JAK ANALIZOWAĆ ZESTAW TRAJEKTORII Z SYMULACJI
+traces = WholeTheory('aa', 21)
+traces.plot_all()
+print(traces.rupture_table)
+traces.draw_rupture_histo()
+print(traces.rupture_table)
+res = traces.crooks()
+print(res)
