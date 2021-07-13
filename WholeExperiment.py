31ac3a7b30c8dbd9308eeb9e471d37376d857393
olagajkowska <67837503+olagajkowska@users.noreply.github.com>
2021-07-13 17:09:05 +0200

    Update WholeExperiment.py

--- WholeExperiment.py
+++ WholeExperiment.py
@@ -1,6 +1,6 @@
 from Whole import Whole
 from Experiment import Experiment
-from Tools import decompose_histogram
+from Tools import dudko_hummer_szabo
 
 
 class WholeExperiment(Whole):
@@ -10,13 +10,22 @@ class WholeExperiment(Whole):
         self._filename = filename
         self._number_of_traces = number_of_traces
         super().__init__(number_of_traces)
-        self._total_trace = Experiment("total_exp.txt", case=None, p=self.p_mean, k=self.k_mean)
+
+    def _collect_traces(self):
+        return [Experiment(self._filename, i, **self._kwargs) for i in range(1, self._number_of_traces)]
+
+    def _get_total_trace(self):
+        return Experiment("total_exp.txt", case=None, p=self.p_mean, k=self.k_mean)
+
+    def _analyze_total_trace(self):
         self._total_trace.state_boundaries()
         self._total_trace.plot(name="total_exp")
         print(self._total_trace.histo_data)
 
-    def _collect_traces(self):
-        return [Experiment(self._filename, i, **self._kwargs) for i in range(1, self._number_of_traces)]
+    def dudko(self):
+        dhs_table = dudko_hummer_szabo(self.rupture_table, self.states_rup)
+        return dhs_table
+
 
 
 
